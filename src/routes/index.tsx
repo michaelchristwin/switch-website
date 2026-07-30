2a94e12052d3f4cfa4e92b329c0c29045e843270
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-07-30 02:07:13 +0100

    Polishing nav and hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import SwitchLogo from '#/assets/brand-logo.png'
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -7,41 +8,31 @@ function Home() {
     <div className="bg-surface font-body-base text-on-surface technical-grid">
       <header className="fixed top-0 w-full z-50 bg-surface/90 backdrop-blur-md border-b border-outline-variant">
         <div className="h-20 max-w-7xl mx-auto px-margin-desktop flex items-center justify-between">
-          <div className="flex items-center gap-4">
+          <Link to="/">
             <img
               alt="Profile"
-              className="w-10 h-10 rounded-full object-cover border border-outline"
-              src="https://lh3.googleusercontent.com/aida/AP1WRLsO2wVqpez-vOeWjJDYZAygE2D5Xgmjv19nRfRgVvLmaut4T76hEGkYgR4-0ysnyktaiFUR16uNqFlV0OTBhUnINeBOxU13IKKjTwJIKZ2fUC_Rq9GmOhfSqpWwdhwRMSPXeOpbVWxP2yb3g7Ru2x1dnr_LiMGKWMavJoZmz7rUjPvZV2seru813kdaVFcwazdw-WYuR37F5BkiPKp4Tq0W0IwY4UJ1Z1lwcKwvMSS4iY4ee243CyVEqw"
-            />{' '}
-            <span className="font-label-caps text-label-caps text-primary">
-              WHYNOTSWITCH.V2
-            </span>
-          </div>
+              className="w-14 h-14 rounded-full object-cover"
+              src={SwitchLogo}
+            />
+          </Link>
           <nav
             className="hidden lg:flex items-center gap-8"
             data-active-classes="text-primary border-b-2 border-primary"
           >
-            <a
+            <Link
               aria-current="page"
               className="font-label-caps transition-colors tracking-widest text-primary border-b-2 border-primary"
               data-path="home"
-              href="#"
+              to="/"
             >
               HOME
-            </a>
+            </Link>
             <a
               className="font-label-caps text-label-caps text-on-surface-variant hover:text-primary transition-colors tracking-widest"
               data-path="product"
               href="#"
             >
-              PRODUCT
-            </a>
-            <a
-              className="font-label-caps text-label-caps text-on-surface-variant hover:text-primary transition-colors tracking-widest"
-              data-path="catalog"
-              href="#"
-            >
-              CATALOG
+              PRODUCT CATALOG
             </a>
             <a
               className="font-label-caps text-label-caps text-on-surface-variant hover:text-primary transition-colors tracking-widest"
@@ -50,30 +41,40 @@ function Home() {
             >
               DATA SHEET
             </a>
-            <a
-              className="font-label-caps text-label-caps text-on-surface-variant hover:text-primary transition-colors tracking-widest"
-              data-path="about"
-              href="#"
-            >
-              ABOUT
-            </a>
-            <a
-              className="font-label-caps text-label-caps text-on-surface-variant hover:text-primary transition-colors tracking-widest"
-              data-path="faq"
-              href="#"
-            >
-              FAQ
-            </a>
           </nav>
           <div className="flex items-center gap-4">
             <a
+              target="_blank"
               className="bg-secondary text-on-secondary px-6 py-3 font-label-caps text-label-caps flex items-center gap-2 hover:bg-on-secondary-container transition-all"
-              href="#"
+              href="https://api.whatsapp.com/send?phone=2348081147003"
             >
               WHATSAPP{' '}
-              <span className="material-symbols-outlined text-[16px]">
-                chat_bubble
-              </span>
+              <svg
+                viewBox="0 0 24 24"
+                width={24}
+                height={24}
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M6.014 8.00613C6.12827 7.1024 7.30277 5.87414 8.23488 6.01043L8.23339 6.00894C9.14051 6.18132 9.85859 7.74261 10.2635 8.44465C10.5504 8.95402 10.3641 9.4701 10.0965 9.68787C9.7355 9.97883 9.17099 10.3803 9.28943 10.7834C9.5 11.5 12 14 13.2296 14.7107C13.695 14.9797 14.0325 14.2702 14.3207 13.9067C14.5301 13.6271 15.0466 13.46 15.5548 13.736C16.3138 14.178 17.0288 14.6917 17.69 15.27C18.0202 15.546 18.0977 15.9539 17.8689 16.385C17.4659 17.1443 16.3003 18.1456 15.4542 17.9421C13.9764 17.5868 8 15.27 6.08033 8.55801C5.97237 8.24048 5.99955 8.12044 6.014 8.00613Z"
+                    fill="#ffffff"
+                  ></path>
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 23C10.7764 23 10.0994 22.8687 9 22.5L6.89443 23.5528C5.56462 24.2177 4 23.2507 4 21.7639V19.5C1.84655 17.492 1 15.1767 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM6 18.6303L5.36395 18.0372C3.69087 16.4772 3 14.7331 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C11.0143 21 10.552 20.911 9.63595 20.6038L8.84847 20.3397L6 21.7639V18.6303Z"
+                    fill="#ffffff"
+                  ></path>
+                </g>
+              </svg>
             </a>
           </div>
         </div>
@@ -88,9 +89,6 @@ function Home() {
             <div className="absolute bottom-margin-desktop right-margin-desktop crosshair crosshair-br pointer-events-none"></div>
             <div className="max-w-7xl mx-auto px-margin-desktop w-full grid grid-cols-1 lg:grid-cols-12 gap-12 relative z-10 pt-20">
               <div className="col-span-1 lg:col-span-5 flex flex-col justify-center space-y-8 relative">
-                <div className="absolute -left-8 top-1/2 -translate-y-1/2 font-label-caps text-[10px] text-outline [writing-mode:vertical-rl] tracking-widest uppercase hidden lg:block">
-                  CUMULUS_SERIES // v2.4.1
-                </div>
                 <div>
                   <span className="inline-block bg-surface-container-high text-on-surface-variant font-label-caps text-label-caps px-3 py-1 mb-6 border border-outline">
                     SYS_ONLINE // GRID_SYNC_ACTIVE
@@ -115,7 +113,7 @@ function Home() {
                     href="#"
                   >
                     <span className="relative z-10 flex items-center gap-2">
-                      INITIATE_WHATSAPP
+                      CHAT ON WHATSAPP
                       <span className="material-symbols-outlined text-sm">
                         chat_bubble
                       </span>
@@ -126,7 +124,7 @@ function Home() {
                     className="bg-surface text-on-surface font-label-caps text-label-caps px-8 py-4 flex items-center justify-center gap-3 border border-outline hover:bg-surface-container transition-colors"
                     href="#demo"
                   >
-                    SEE_CUMULUS_IN_ACTION
+                    SEE CUMULUS IN ACTION
                     <span className="material-symbols-outlined text-sm">
                       arrow_forward
                     </span>
