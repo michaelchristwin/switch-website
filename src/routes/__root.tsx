2175696cfa551c02eaf97e67b13519ac5ac5ae77
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-08-10 20:41:48 +0100

    Removed canonical links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -106,10 +106,10 @@ export const Route = createRootRoute({
         href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0',
         rel: 'stylesheet',
       },
-      {
-        rel: 'canonical',
-        href: 'https://whynotswitch.com/',
-      },
+      // {
+      //   rel: 'canonical',
+      //   href: 'https://whynotswitch.com/',
+      // },
     ],
     scripts: [
       {
